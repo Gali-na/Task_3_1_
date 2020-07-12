16010f78a0bbe33292cb0618beabb0dfe14baad2
Tsivka Galina <galinatsivka@gmail.come>
2020-07-12 23:49:14 +0300

    desision

--- src/main/java/SqrtOperation.java
+++ src/main/java/SqrtOperation.java
@@ -1,5 +1,30 @@
+/**
+ * <p>Реализуйте метод sqrt(), вычисляющий квадратный корень числа. В отличие от Math.sqrt(),
+ * это метод при передаче отрицательного параметра должен бросать исключение
+ * java.lang.IllegalArgumentException с сообщением "Expected non-negative number, got ?",
+ * где вместо вопросика будет подставлено фактически переданное в метод число.</p>
+ */
+
 public class SqrtOperation {
     public double sqrt(double number) {
-        return 0.0;
+        if (number < 0) {
+            throw new IllegalArgumentException("Expected non-negative number, got " + number);
+        }
+        double squareRoot = 1.0;
+        double check = 0.0;
+        for (int i = 0; i <= 100; i++) {
+            if (number == 0) {
+                squareRoot = 0.0;
+                break;
+            }
+            squareRoot = squareRoot - (squareRoot * squareRoot - number) / (2 * squareRoot);
+            if (i != 0) {
+                if (check - squareRoot < 0.00000001) {
+                    break;
+                }
+            }
+            check = squareRoot;
+        }
+        return squareRoot;
     }
 }
